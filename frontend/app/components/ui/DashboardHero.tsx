2ed69b53610f1aac234fc501f725e3720e4a70c3
Donald Philp <w2107579@westminster.ac.uk>
2025-06-09 08:50:45 -0400

    hero mobile margins fix

--- frontend/app/components/ui/DashboardHero.tsx
+++ frontend/app/components/ui/DashboardHero.tsx
@@ -50,7 +50,7 @@ const DashboardHero: React.FC<HeroProps> = ({ darkMode = false }) => {
   return (
     <div className={bgColor}>
       <div className="relative isolate overflow-hidden bg-hero-gradient">
-        <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-7xl pb-24 pt-10 px-8 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:py-40">
           <div className="px-6 lg:px-0 lg:pt-4">
             <div className="mx-auto max-w-2xl">
               <div className="max-w-lg">
--- frontend/app/components/ui/HeroSectionTop.tsx
+++ frontend/app/components/ui/HeroSectionTop.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
   return (
     <div className="bg-background">
       <div className="relative isolate overflow-hidden bg-hero-gradient">
-        <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-7xl pb-24 pt-10 px-8 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:py-40">
           <div className="px-6 lg:px-0 lg:pt-4">
             <div className="mx-auto max-w-2xl">
               <div className="max-w-lg">
